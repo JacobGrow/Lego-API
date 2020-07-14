6e1f1f0330e19c874fd57d29efe27fdb8f1f74f4
Jacob Grow <jacobagrow@gmail.com>
2020-07-14 17:05:53 -0600

    Added Brick Service

--- Controllers/BricksController.cs
+++ Controllers/BricksController.cs
@@ -8,11 +8,84 @@ namespace CS_Lego.Controllers
 
     [Route("api/[controller]")]
     [ApiController]
-    public class BricksController
+    public class BricksController : ControllerBase
     {
-       private readonly BrickService : BaseController
+       private readonly BrickService _service;
+       public BricksController(BrickService service)
        {
-           
+        _service = service;
        } 
+
+       //Get
+       [HttpGet]
+       public ActionResult<IEnumerable<Brick>> Get()
+       {
+           try
+           {
+               return Ok(_service.Get());
+           }
+           catch (Exception e)
+           {
+               return BadRequest(e.Message);
+           }
+       }
+
+       //GetById
+       [HttpGet("{Id}")]
+       public ActionResult<Brick> Get(int Id)
+       {
+             try
+           {
+               return Ok(_service.Get(Id));
+           }
+           catch (Exception e)
+           {
+               return BadRequest(e.Message);
+           }
+       }
+
+        //Post
+       [HttpPost]
+       public ActionResult<Brick> Post([FromBody] Brick newBrick)
+       {
+             try
+           {
+               return Ok(_service.Create(newBrick));
+           }
+           catch (Exception e)
+           {
+               return BadRequest(e.Message);
+           }
+       }
+
+       //Put
+        [HttpPut("{id}")]
+        public ActionResult<Brick> Edit([FromBody] Brick newBrick, int id)
+        {
+              try
+           {
+               newBrick.Id = id;
+               return Ok(_service.Edit(newBrick));
+           }
+           catch (Exception e)
+           {
+               return BadRequest(e.Message);
+           }
+        }
+
+        //Delete
+        [HttpDelete("{id}")]
+        public ActionResult<Brick> Delete(int id)
+        {
+              try
+           {
+               return Ok(_service.Delete(id));
+           }
+           catch (Exception e)
+           {
+               return BadRequest(e.Message);
+           }
+        }
+
     }
 }
--- Services/BrickService.cs
+++ Services/BrickService.cs
@@ -1,7 +1,33 @@
+using System;
+using CS_Lego.Models;
+
 namespace CS_Lego.Services
 {
-    public class BrickService
+  public class BrickService
+  {
+    internal object Get(object id)
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal object Create(Brick newBrick)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Edit(Brick newBrick)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Get()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
