01cd886c4e7c851ed369335c7d922e9e71d70912
Jacob Grow <jacobagrow@gmail.com>
2020-07-14 15:14:31 -0600

    Built Kit Service

--- Repositories/KitRepository.cs
+++ Repositories/KitRepository.cs
@@ -1,7 +1,34 @@
+using System;
+using System.Collections.Generic;
+using CS_Lego.Models;
+
 namespace CS_Lego.Repositories
 {
-    public class LegoKitRepository
+  public class KitRepository
+  {
+    internal IEnumerable<Kit> Get()
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal Kit GetById(int kitId)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal int Create(Kit newKit)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Edit(Kit original)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
--- Services/KitService.cs
+++ Services/KitService.cs
@@ -1,33 +1,52 @@
 using System;
+using System.Collections.Generic;
 using CS_Lego.Models;
+using CS_Lego.Repositories;
 
 namespace CS_Lego.Services
 {
+ 
   public class KitService
   {
-    internal object Get()
+
+    private readonly KitRepository _repo;
+    public KitService(KitRepository repo)
     {
-      throw new NotImplementedException();
+      _repo = repo;
+    }
+    public IEnumerable<Kit> Get()
+    {
+      return _repo.Get();
     }
 
-    internal object Get(int kitId)
+    public Kit Get(int kitId)
     {
-      throw new NotImplementedException();
+      Kit exists = _repo.GetById(kitId);
+      if(exists==null) { throw new Exception("Invalid Id DumDum."); }
+      return exists;
     }
 
-    internal object Create(Kit newKit)
+    public Kit Create(Kit newKit)
     {
-      throw new NotImplementedException();
+      int id = _repo.Create(newKit);
+      newKit.Id = id;
+      return newKit;
     }
 
-    internal object Edit(Kit newKit)
+    internal object Edit(Kit editKit)
     {
-      throw new NotImplementedException();
+     Kit original = Get(editKit.Id);
+     original.Name = editKit.Name.Length > 0 ? editKit.Name : original.Name;
+     original.Price = editKit.Price > 0 ? editKit.Price : original.Price;
+     original.Description = editKit.Description.Length > 0 ? editKit.Description : original.Description;
+     return _repo.Edit(original);
     }
 
     internal object Delete(int id)
     {
-      throw new NotImplementedException();
+      Kit exists = Get(id);
+      _repo.Delete(id);
+      return exists;
     }
   }
 }
